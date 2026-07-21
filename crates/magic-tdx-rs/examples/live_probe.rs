cedc4c47fc316997726f788dd175b223188fd104
Northofqing <magic.automation.ian@gmail.com>
2026-07-22 00:15:45 +0800

    test(tdx): probe live quote retrieval

--- crates/magic-tdx-rs/examples/live_probe.rs
+++ crates/magic-tdx-rs/examples/live_probe.rs
@@ -2,7 +2,13 @@ use magic_tdx_rs::TdxSmartClient;
 fn main() {
     let client = TdxSmartClient::new();
     match client.connect_to_any(Some(3.0)) {
-        Ok(true) => println!("connected=true"),
+        Ok(true) => {
+            println!("connected=true");
+            match client.get_security_quotes(&[(1, "600519")]) {
+                Ok(quotes) => println!("quotes={} first_price={}", quotes.len(), quotes.first().map_or(0.0, |q| q.price)),
+                Err(error) => println!("quotes=error error={error}"),
+            }
+        }
         Ok(false) => println!("connected=false"),
         Err(error) => println!("connected=error error={error}"),
     }
