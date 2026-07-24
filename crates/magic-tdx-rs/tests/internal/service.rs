f8f5918b737f5e1045243e659958080bc26a916d
Northofqing <magic.automation.ian@gmail.com>
2026-07-24 08:46:12 +0800

    test(tdx): specify blocking service query seam

--- crates/magic-tdx-rs/tests/internal/service.rs
+++ crates/magic-tdx-rs/tests/internal/service.rs
@@ -2,9 +2,133 @@ use super::*;
 use magic_market_core::{
     AssetClass, BarInterval, Exchange, InstrumentId, MinuteDataRequest, TradesRequest,
 };
+use std::cell::RefCell;
+use std::collections::VecDeque;
 
 fn instrument(exchange: Exchange, code: &str) -> InstrumentId {
     InstrumentId::new(exchange, code, AssetClass::Equity).unwrap()
+}
+
+#[derive(Default)]
+struct ScriptedBlockingServiceQuery {
+    count_calls: RefCell<Vec<u8>>,
+    count_responses: RefCell<VecDeque<Result<u16, TdxError>>>,
+    list_calls: RefCell<Vec<(u8, u16)>>,
+    list_responses: RefCell<VecDeque<Result<Vec<SecurityInfo>, TdxError>>>,
+}
+
+fn unconfigured<T>(operation: &str) -> Result<T, TdxError> {
+    Err(TdxError::InvalidData(format!(
+        "scripted {operation} response is not configured"
+    )))
+}
+
+impl crate::adapter::BlockingTdxQuery for ScriptedBlockingServiceQuery {
+    fn security_bars(
+        &self,
+        _category: u8,
+        _market: u8,
+        _code: &str,
+        _start: u32,
+        _count: u16,
+        _adjust: u8,
+    ) -> Result<Vec<SecurityBar>, TdxError> {
+        unconfigured("bars")
+    }
+
+    fn security_quotes(
+        &self,
+        _instruments: &[(u8, &str)],
+    ) -> Result<Vec<SecurityQuote>, TdxError> {
+        unconfigured("quotes")
+    }
+
+    fn minute_time_data(
+        &self,
+        _market: u8,
+        _code: &str,
+    ) -> Result<Vec<MinuteTimePrice>, TdxError> {
+        unconfigured("current minute")
+    }
+
+    fn history_minute_time_data(
+        &self,
+        _market: u8,
+        _code: &str,
+        _date: u32,
+    ) -> Result<Vec<MinuteTimePrice>, TdxError> {
+        unconfigured("history minute")
+    }
+
+    fn transaction_data(
+        &self,
+        _market: u8,
+        _code: &str,
+        _start: u16,
+        _count: u16,
+    ) -> Result<Vec<TickData>, TdxError> {
+        unconfigured("current transactions")
+    }
+
+    fn history_transaction_data(
+        &self,
+        _market: u8,
+        _code: &str,
+        _start: u16,
+        _count: u16,
+        _date: u32,
+    ) -> Result<Vec<TickData>, TdxError> {
+        unconfigured("history transactions")
+    }
+
+    fn security_count(&self, market: u8) -> Result<u16, TdxError> {
+        self.count_calls.borrow_mut().push(market);
+        self.count_responses
+            .borrow_mut()
+            .pop_front()
+            .unwrap_or_else(|| unconfigured("security count"))
+    }
+
+    fn security_list(&self, market: u8, start: u16) -> Result<Vec<SecurityInfo>, TdxError> {
+        self.list_calls.borrow_mut().push((market, start));
+        self.list_responses
+            .borrow_mut()
+            .pop_front()
+            .unwrap_or_else(|| unconfigured("security list"))
+    }
+}
+
+fn security(code: usize) -> SecurityInfo {
+    SecurityInfo {
+        code: format!("{code:06}"),
+        volunit: 100,
+        decimal_point: 2,
+        name: format!("证券{code}"),
+        pre_close: 10.0,
+    }
+}
+
+#[test]
+fn blocking_service_security_list_seam_assembles_declared_count_atomically() {
+    let query = ScriptedBlockingServiceQuery {
+        count_responses: RefCell::new(VecDeque::from([Ok(2001)])),
+        list_responses: RefCell::new(VecDeque::from([
+            Ok((0..1000).map(security).collect()),
+            Ok((1000..2000).map(security).collect()),
+            Ok(vec![security(2000)]),
+        ])),
+        ..Default::default()
+    };
+
+    let records = security_list_all_with(&query, 1).unwrap();
+
+    assert_eq!(records.len(), 2001);
+    assert_eq!(*query.count_calls.borrow(), vec![1]);
+    assert_eq!(
+        *query.list_calls.borrow(),
+        vec![(1, 0), (1, 1000), (1, 2000)]
+    );
+    assert_eq!(records[2000].code, "002000");
 }
 
 #[test]
